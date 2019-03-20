ffd74b74a3eeb55a4c7e7df5c419d97ecc13ca78
Ahmed Tarek <ahmed-tarek94@hotmail.com>
2019-03-20 13:47:05 +0200

    added the price functionality

--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -3,6 +3,14 @@ import AUX from '../hoc/auxilary'
 import Burger from '../components/Burger/Burger'
 import BuildControls from '../components/Burger/BuildControls/BuildControls'
 
+
+const INGREDIANTS_PRICIES = {
+    salad : 0.3,
+    bacon : 0.7,
+    cheese: 0.5,
+    meat : 1.5
+}
+
 class BurgerBuilder extends Component {
     state = {
         ingrediants  : {
@@ -10,21 +18,40 @@ class BurgerBuilder extends Component {
             bacon : 0,
             cheese : 0,
             meat : 2
-        }
+        },
+        price: 4
     }
 
+    addPrice = (oldPrice, type) => {
+        let price = oldPrice;
+        price = price + INGREDIANTS_PRICIES[type]
+        return price;
+    }
+
+     removePrice = (oldPrice, type) => {
+        let price = oldPrice;
+        price = price - INGREDIANTS_PRICIES[type]
+        return price;
+    }
     ingrediantsControl= (type, isIncrease) => {
 
         this.setState((prevState,props)=> {
             let modifiedState = prevState.ingrediants
+            let newPrice=null;
             if(isIncrease){
                 modifiedState[type] = modifiedState[type]+1;
+                 newPrice = this.addPrice(prevState.price,type)
             } else if(!isIncrease && modifiedState[type]>0){
                 modifiedState[type] = modifiedState[type]-1;
+                newPrice = this.removePrice(prevState.price,type)
+
+            } else {
+                newPrice = prevState.price;
             }
 
             return {
-                ingrediants : modifiedState
+                ingrediants : modifiedState,
+                price : newPrice
             }
         })
     }
